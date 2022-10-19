eb568439905ef7e03ee373e9bd62484c31754f6f
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-10-19 17:12:21 +0000

    Adding console options

---
+++ graphconsoleapp/M365ConsoleOptions.cs
@@ -0,0 +1,8 @@
+namespace graphconsoleapp;
+
+public class M365ConsoleOptions
+{
+    public string? ApplicationID { get; set; }
+    public string? ApplicationSecret { get; set; }
+    public string? TenantID { get; set; }
+}
