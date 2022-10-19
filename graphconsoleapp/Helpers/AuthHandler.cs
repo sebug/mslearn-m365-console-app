963ec317bb16be7721347e4182d05fb11ba8dd7c
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-10-19 17:23:51 +0000

    Adding Auth handler

---
+++ graphconsoleapp/Helpers/AuthHandler.cs
@@ -0,0 +1,20 @@
+using Microsoft.Graph;
+
+namespace graphconsoleapp.Helpers;
+
+public class AuthHandler : DelegatingHandler
+{
+    private readonly IAuthenticationProvider _authenticationProvider;
+    
+    public AuthHandler(IAuthenticationProvider authenticationProvider, HttpMessageHandler innerHandler)
+    {
+        InnerHandler = innerHandler;
+        _authenticationProvider = authenticationProvider;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        await _authenticationProvider.AuthenticateRequestAsync(request);
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
