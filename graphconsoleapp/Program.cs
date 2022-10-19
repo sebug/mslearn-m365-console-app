33ff65f7b9ceeb9efd346da01b88cf5b318d8b50
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-10-19 17:18:06 +0000

    Environment config variables

--- graphconsoleapp/Program.cs
+++ graphconsoleapp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using graphconsoleapp;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .Build();
+
+var settings = config.GetRequiredSection("M365Console").Get<M365ConsoleOptions>();
+
+Console.WriteLine("Hello, " + settings.TenantID);
