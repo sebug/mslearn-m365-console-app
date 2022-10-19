04c8fa391315961e7e207d2113364a120b3886c7
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-10-19 20:40:44 +0000

    Program does something

--- graphconsoleapp/Program.cs
+++ graphconsoleapp/Program.cs
@@ -1,6 +1,9 @@
 ﻿
 using graphconsoleapp;
+using graphconsoleapp.Helpers;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
 
 var config = new ConfigurationBuilder()
     .AddEnvironmentVariables()
@@ -8,4 +11,38 @@ var config = new ConfigurationBuilder()
 
 var settings = config.GetRequiredSection("M365Console").Get<M365ConsoleOptions>();
 
-Console.WriteLine("Hello, " + settings.RedirectUri);
+var client = GetAuthenticatedGraphClient(settings);
+
+var graphRequest = client.Users.Request();
+
+var results = await graphRequest.GetAsync();
+
+foreach (var user in results)
+{
+    Console.WriteLine($"{user.Id}: {user.DisplayName} <{user.Mail}>");
+}
+Console.WriteLine(Environment.NewLine + "Graph Request:");
+Console.WriteLine(graphRequest.GetHttpRequestMessage().RequestUri);
+
+GraphServiceClient GetAuthenticatedGraphClient(M365ConsoleOptions options)
+{
+    var authenticationProvider = CreateAuthorizationProvider(options);
+    var graphClient = new GraphServiceClient(authenticationProvider);
+    return graphClient;
+}
+
+IAuthenticationProvider CreateAuthorizationProvider(M365ConsoleOptions options)
+{
+    var scopes = new List<string>()
+    {
+        "https://graph.microsoft.com/.default"
+    };
+    var authority = $"https://login.microsoftonline.com/{options.TenantID}/v2.0";
+    var cca = ConfidentialClientApplicationBuilder.Create(options.ApplicationID)
+        .WithAuthority(authority)
+        .WithRedirectUri(options.RedirectUri)
+        .WithClientSecret(options.ApplicationSecret)
+        .Build();
+    
+    return new MsalAuthenticationProvider(cca, scopes.ToArray());
+}
